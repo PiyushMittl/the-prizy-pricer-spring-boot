b13ba4fa8d045e1ea64681210c3a53780b243cd4
piyushmittl <mr.piyushmittal@yahoo.com>
2018-02-06 17:41:01 +0530

    initial

--- src/main/java/com/boraji/tutorial/springboot/MainApp.java
+++ src/main/java/com/boraji/tutorial/springboot/MainApp.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @ComponentScan({"com.piyush","com.boraji"})
 @EntityScan("com.piyush.springboot.entity")
+@EnableScheduling
 @SpringBootApplication
 public class MainApp {
    public static void main(String[] args) {
